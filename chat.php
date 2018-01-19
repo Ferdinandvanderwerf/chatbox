7ccbaebfc9e9ed3f6e9c48891b477f0528d5743a
Ferdinandvanderwerf <34487619+Ferdinandvanderwerf@users.noreply.github.com>
2018-01-19 15:58:21 +0100

    Insert files

---
+++ chat.php
@@ -0,0 +1,17 @@
+<?php
+	include 'db.php';
+			
+	$query = "SELECT * FROM chat ORDER BY id DESC";
+	$run = $con->query($query);
+			
+	while($row = $run->fetch_array()) :
+?>
+			
+		<div class="myTest" id="chat_data">
+				<span style="color:green;"> <?php echo $row['name']; ?> </span> :
+				<span style="color:brown;"> <?php echo $row['msg']; ?> </span>	
+				<span style="float:right;"> <?php echo $row['date']; ?> </span>					
+		</div>
+<?php 
+	endwhile;
+?>
---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pass = "java1234";
+	$db_name = "chat";
+
+	$con = new mysqli($host,$user,$pass,$db_name);
+?>
